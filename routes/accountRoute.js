2e1fe284a2e070d3dca8c9d9baf60bd49e28fe16
Seth McNabb <sethmcnabb00@gmail.com>
2025-06-20 16:15:10 -0600

    .

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,12 +1,21 @@
-const express = require("express")
-const router = new express.Router() 
-const accountController = require("../controllers/accountController")
-const utilities = require("../utilities")
+const regValidate = require('../utilities/account-validation')
+const express = require('express');
+const router = express.Router();
+const utilities = require('../utilities');
+const accountController = require('../controllers/accountController');
 
-// from inventoryRoute
-router.get(
-  "/",
-  utilities.handleErrors(accountController.buildLogin)
-);
+// Login Routes
+router.get('/login', utilities.handleErrors(accountController.buildLogin()))
+
+// Registration Routes
+router.get('/register', utilities.handleErrors(accountController.buildRegister()))
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router;
+
